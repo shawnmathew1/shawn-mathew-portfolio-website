8586678c5392ee15edfbbbbca03fec0346b161b7
shawnmathew1 <shawneapenmathew@gmail.com>
2026-01-25 20:18:59 -0500

    JS Variables Added

---
+++ landingScript.js
@@ -0,0 +1,6 @@
+const titles = ["Fullstack Developer", "Frontend Developer", "Programmer"]
+const typingElement = document.getElementById("typing");
+let i = 0;
+let j = 0;
+let currentSkill = "";
+let isDeleting = false;
